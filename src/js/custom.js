2442e2bc1268ae8fe4087d4353f336010effe186
Sameera <www.sameera@gmail.com>
2024-05-18 12:38:24 -0400

    fix: img urls

--- src/js/custom.js
+++ src/js/custom.js
@@ -19,11 +19,4 @@
     });
 }(jQuery));
 
-document.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
+
